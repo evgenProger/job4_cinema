4a00cddcb74c1d7c5ceaf0b7f6b90aa37c7a62c2
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2022-10-18 17:55:49 +0500

    Create project

---
+++ src/main/java/ru/job4j/model/Ticket.java
@@ -0,0 +1,53 @@
+package ru.job4j.model;
+
+import java.util.Objects;
+
+public class Ticket {
+
+    private int id;
+    private int row;
+    private int cell;
+
+    public Ticket(int id, int row, int cell) {
+        this.id = id;
+        this.row = row;
+        this.cell = cell;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCell() {
+        return cell;
+    }
+
+    public void setCell(int cell) {
+        this.cell = cell;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ticket ticket = (Ticket) o;
+        return id == ticket.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
