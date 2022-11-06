987867b347a5b707bd533dd4a0f755baab180f13
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2022-11-06 19:46:59 +0500

    Add createSeesion method

--- src/main/java/ru/job4j/cinema/Main.java
+++ src/main/java/ru/job4j/cinema/Main.java
@@ -11,6 +11,7 @@ import java.util.Properties;
 
 @SpringBootApplication
 public class Main {
+
     private Properties loadDbProperties() {
         Properties cfg = new Properties();
         try (BufferedReader io = new BufferedReader(
@@ -31,19 +32,19 @@ public class Main {
         return cfg;
     }
 
-//    @Bean
-//    public BasicDataSource loadPool() {
-//        Properties cfg = loadDbProperties();
-//        BasicDataSource pool = new BasicDataSource();
-//        pool.setDriverClassName(cfg.getProperty("jdbc.driver"));
-//        pool.setUrl(cfg.getProperty("jdbc.url"));
-//        pool.setUsername(cfg.getProperty("jdbc.username"));
-//        pool.setPassword(cfg.getProperty("jdbc.password"));
-//        pool.setMinIdle(5);
-//        pool.setMaxIdle(10);
-//        pool.setMaxOpenPreparedStatements(100);
-//        return pool;
-//    }
+    @Bean
+    public BasicDataSource loadPool() {
+        Properties cfg = loadDbProperties();
+        BasicDataSource pool = new BasicDataSource();
+        pool.setDriverClassName(cfg.getProperty("jdbc.driver"));
+        pool.setUrl(cfg.getProperty("jdbc.url"));
+        pool.setUsername(cfg.getProperty("jdbc.username"));
+        pool.setPassword(cfg.getProperty("jdbc.password"));
+        pool.setMinIdle(5);
+        pool.setMaxIdle(10);
+        pool.setMaxOpenPreparedStatements(100);
+        return pool;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
---
+++ src/main/java/ru/job4j/cinema/controller/IndexControl.java
@@ -0,0 +1,71 @@
+package ru.job4j.cinema.controller;
+
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.job4j.cinema.model.Session;
+import ru.job4j.cinema.service.SessionService;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class IndexControl {
+    private final SessionService sessionService;
+
+    public IndexControl(SessionService sessionService) {
+        this.sessionService = sessionService;
+    }
+
+    @GetMapping("/index")
+    public String sessions(Model model) {
+        model.addAttribute("sessions", sessionService.findAll());
+        return "index";
+    }
+
+    @PostMapping("/createSession")
+    public String createSession(@ModelAttribute Session session,
+                                  @RequestParam("file") MultipartFile file) throws IOException {
+        sessionService.add(session);
+        String ext = FilenameUtils.getExtension(file.getOriginalFilename());
+        File photo = new File("src/main/resources/films"
+                + File.separator
+                + session.getId()
+                + "."
+                + ext);
+        photo.createNewFile();
+        try (FileOutputStream out = new FileOutputStream(photo)) {
+            out.write(file.getBytes());
+        }
+        return "redirect:/index";
+    }
+
+    @GetMapping("/poster/{sessionId}")
+    public ResponseEntity<Resource> download(@PathVariable("sessionId") Integer sessionId) throws IOException {
+        Session session = sessionService.findById(sessionId);
+        String photoName = Arrays.stream(Objects.requireNonNull(new File("src/main/resources/films")
+                        .listFiles())).filter(f ->
+                        f.getName().split("\\.")[0].equals(String.valueOf(sessionId)))
+                .findFirst().get().getName();
+        File photo = new File("src/main/resources/films" + File.separator + photoName);
+        byte[] arr = Files.readAllBytes(photo.toPath());
+        return ResponseEntity.ok()
+                .headers(new HttpHeaders())
+                .contentLength(arr.length)
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(new ByteArrayResource(arr));
+    }
+
+
+
+}
